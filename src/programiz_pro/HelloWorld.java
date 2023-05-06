a18eec5ee02193adc349ee713d1337aaa6a302d9
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-05-07 00:56:56 +0530

    Commit FR

--- src/programiz_pro/HelloWorld.java
+++ src/programiz_pro/HelloWorld.java
@@ -3,7 +3,8 @@ package programiz_pro;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("welcome to programiz_pro");
+		
 
 	}
 
