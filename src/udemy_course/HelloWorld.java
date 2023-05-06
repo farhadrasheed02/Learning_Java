52fbe32dbcc0c88f3abeaf1a8670763b5eb4ccb8
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-05-07 00:49:51 +0530

    Initial commit

---
+++ src/udemy_course/HelloWorld.java
@@ -0,0 +1,10 @@
+package udemy_course;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Finally started the course");
+	}
+
+}
