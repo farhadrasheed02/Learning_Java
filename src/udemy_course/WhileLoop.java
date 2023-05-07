700285aab486e012b34c74df45880694c1d99527
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-05-07 18:44:07 +0530

    Commit by FR

---
+++ src/udemy_course/WhileLoop.java
@@ -0,0 +1,14 @@
+package udemy_course;
+public class WhileLoop {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int mynum= 0;
+		while(mynum<10)
+		{
+			System.out.println("hello world");
+			mynum=mynum+1;
+		}
+
+	}
+
+}
