34f6a465a0d45e935fecce3bc9b226f3a083dbac
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-05-12 00:48:33 +0530

    Commit FR

--- src/udemy_course/ForEachLoop.java
+++ src/udemy_course/ForEachLoop.java
@@ -4,7 +4,7 @@ package udemy_course;
 		public static void main(String[] args) {
 			String  [] cars = {"bugatti","audi","Range Rover","Jeep","Baleno","Swift Desire"};
 			Integer [] abc = {1,2,3,4};
-			for(String i : cars)
+			for(String i : cars)    // use of the for each loop
 			{
 				System.out.println(i);
 			}
---
+++ src/udemy_course/MultiDimArray.java
@@ -0,0 +1,20 @@
+package udemy_course;
+
+public class MultiDimArray {
+	public static void main(String[] args) {
+		int [][] myNum = {{1,2,4,6,8},{4,10,12,14,59}};
+		System.out.println(myNum[0][4]);
+	/*	for(int i=0; i<myNum.length;i++)
+		{
+			for(int j=0;j<myNum[i].length;j++)
+			{
+				System.out.println(myNum[i][j]);
+			}
+			
+		}
+		*/
+		}
+	
+
+
+}
