83d157c4258d7f0de69cf3e18c6d7da324dc3085
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-05-16 14:39:02 +0530

    Commit By FR

--- src/udemy_course/practice.java
+++ src/udemy_course/practice.java
@@ -4,10 +4,13 @@ public class practice {
 	String txt;
 	public  void speak()
 	{
-		System.out.println("My Name is Farhad");
-		Scanner sc= new Scanner(System.in);
 		System.out.println("Enter the name of the Student");
-		txt = sc.next();
+		//System.out.println("My Name is Farhad");
+		 Scanner sc = new Scanner(System.in);
+		 txt = sc.next();
+		
+			
+		
 		
 		
 		
