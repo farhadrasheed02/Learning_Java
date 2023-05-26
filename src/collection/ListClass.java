04a526f8160c6fe24545de796e3e71d937bdb303
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-05-27 00:35:37 +0530

    Commit By FR

--- src/collection/ListClass.java
+++ src/collection/ListClass.java
@@ -7,12 +7,10 @@ public class ListClass {
 
 	public static void main(String[] args) {
 		List<String> lst = new ArrayList<>();
-		lst.add("farhad");
-		lst.add("haseena");
-		for(String i : lst)
-		{
-			System.out.println("Values of the List are : - "+i);
-		}
+		lst.add("Farhad");
+		lst.add("Haseena");
+		lst.add("Parent");
+		System.out.println(lst);
 	}
 
 }
--- src/xml_parsing/ParsingXml.java
+++ src/xml_parsing/ParsingXml.java
@@ -4,13 +4,15 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import javax.swing.text.Document;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class ParsingXml extends UtilityClass {
 
@@ -18,14 +20,37 @@ public class ParsingXml extends UtilityClass {
 		//Path filepath = Path.of("C:\\Users\\Farhad Rashid Reshi\\eclipse-workspace\\Learning_Java\\src\\xml_parsing\\XFile.xml");
 		
 		try
-		{String filepath = "C:\\Users\\Farhad Rashid Reshi\\eclipse-workspace\\Learning_Java\\src\\xml_parsing\\XFile.xml";
-		File inputFile = new File(filepath);
+		{
+			String filepath = ("\\Learning_Java\\src/xml_parsing\\laptop.xml");
+			//String filepath = "C:\\Users\\Farhad Rashid Reshi\\eclipse-workspace\\Learning_Java\\src\\xml_parsing\\XFile.xml";
+			File inputFile = new File(filepath);
 
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dBuilder = dbf.newDocumentBuilder();
-		Document document = (Document) dBuilder.parse(inputFile);
-		((Path) document.getDefaultRootElement()).normalize();
-		System.out.println("Root element :" + document.getDefaultRootElement().getName());
+		Document document = dBuilder.parse(inputFile);
+		document.getDocumentElement().normalize();
+		NodeList ndlist = document.getElementsByTagName("laptop");
+	 for(int i=0;i<ndlist.getLength();i++)
+	 {
+		Node nd =ndlist.item(i);
+		 if(nd.getNodeType()==Node.ELEMENT_NODE)
+		 {
+			Element nditem = (Element) nd;
+			System.out.println(nditem.getAttribute("name"));
+			NodeList studentdet = nditem.getChildNodes();
+			for(int j=0;j<studentdet.getLength();j++)
+			{
+				Node nda = studentdet.item(j);
+				System.out.println(nda);
+				if(nda.getNodeType()==Node.ELEMENT_NODE)
+				{
+					Element ndaitem = (Element) nda;
+					System.out.println("     " + ndaitem.getTagName() + ": " + ndaitem.getAttribute("value"));
+				}
+			}
+		 }
+		 System.out.println(i);
+	 }
 		}
 		catch(Exception e)
 		{
