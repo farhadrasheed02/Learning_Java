baf116785cc3f2b31468f07fef7f281afdc2f6ad
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-05-28 12:30:42 +0530

    Commit By FR

--- src/collection/ListClass.java
+++ src/collection/ListClass.java
@@ -6,11 +6,25 @@ import java.util.List;
 public class ListClass {
 
 	public static void main(String[] args) {
-		List<String> lst = new ArrayList<>();
-		lst.add("Farhad");
-		lst.add("Haseena");
-		lst.add("Parent");
-		System.out.println(lst);
+		
+		//TypeSafe means that we use generic <String> so we can add only one type of data i.e. in this case String
+		ArrayList<String> array = new ArrayList();
+	//	array.add(5); // voilates the typesafe here
+		array.add("farhad");
+		System.out.println(array);
+		
+		//UnType Safe means we can add different data type like int, string , float etc.
+		
+		ArrayList arr = new ArrayList();
+		arr.add(4); // int
+		arr.add("farhad"); // string
+		arr.add(44.2); //float or double
+		arr.add(4);  // duplicate can be done here as we can see
+		System.out.println(arr);
+		
+		// indexwise data can be achieved in list so we check this property here as well
+		System.out.println(arr.get(2)); // indexing is achieved and we can see order is also preserved here as well
+		
 	}
 
 }
--- src/udemy_course/Strings.java
+++ src/udemy_course/Strings.java
@@ -1,9 +1,28 @@
 package udemy_course;
 	public class Strings {
 		public static void main(java.lang.String[] args) {
-			double myDouble = 85.6;
-			String myText = "farhad";
-			System.out.println("My Name is "+myText +"and my percentage is "+myDouble );
+			//double myDouble = 85.6;
+			int count=0;
+			String myText = "farhad Rashid Reshi Farhad Haseena Khan";
+			char [] arr = myText.toCharArray();
+			for(int i=0;i<arr.length;i++) {
+			if(arr[i] != ' ' && (i == 0  ))
+					{
+					System.out.println(arr[i]);
+				
+					}
+			}
+		//	System.out.println(arr);
+//			for(int i=0;i<myText.length();i++)
+//			{
+//				System.out.print(myText.charAt(i)+" ");
+//				count++;
+//				if(count==6)
+//				{
+//					
+//				}
+//			}
+			//.out.println("My Name is "+myText +"and my percentage is "+myDouble );
 		}
 	
 	}
