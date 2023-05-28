b2af6c0b8a4aa41dac3d7c49555c50f0ae7b5822
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-05-29 01:02:55 +0530

    Commit By FR

---
+++ src/collection/SetClass.java
@@ -0,0 +1,16 @@
+package collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetClass {
+
+	public static void main(String[] args) {
+		HashSet<String> set = new HashSet<>();
+		set.add("Farhad");
+		set.add("Farhad");
+		//it will not duplicate the values....
+		System.out.println(set);
+	}
+
+}
