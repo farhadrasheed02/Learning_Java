ae1b6f2a7abbe60c1fa730e2a591dcfa31bee1ec
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-05-29 00:37:08 +0530

    Commit By FR

---
+++ src/collection/Vectorcla.java
@@ -0,0 +1,15 @@
+package collection;
+
+import java.util.Vector;
+
+public class Vectorcla {
+
+	public static void main(String[] args) {
+		Vector<String> vr = new Vector<String>();
+		vr.add("Farhad");
+		vr.add("haseena");
+		
+
+	}
+
+}
