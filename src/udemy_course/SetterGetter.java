62194dc5a7206691e381443f432f60eb045289c7
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-05-30 23:32:08 +0530

    Commit By FR

--- src/udemy_course/SetterGetter.java
+++ src/udemy_course/SetterGetter.java
@@ -1,9 +1,23 @@
 package udemy_course;
 
 public class SetterGetter {
+	private String name;
+	
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SetterGetter sg = new SetterGetter();
+		sg.setName("Farahd");
+		System.out.println(sg.getName());
 
 	}
 
