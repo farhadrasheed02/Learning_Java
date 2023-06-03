be000326393b04f63154376b4e521e094e14b9e6
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-06-03 12:07:18 +0530

    Commit By FR

--- src/collection/TraverseExample.java
+++ src/collection/TraverseExample.java
@@ -1,8 +1,18 @@
 package collection;
-
+import java.util.ArrayList;
 public class TraverseExample {
 
 	public static void main(String[] args) {
+		
+		//Different Traverse Operations on ArrayList
+		ArrayList<String> arr = new ArrayList<String>();
+		arr.add("4"); //
+		arr.add("farhad"); // string
+		arr.add("44.2"); //float or double
+		arr.add("4");  // duplicate can be done here as we can see
+		System.out.println(arr);
+		
+		//first Traverse Operation on ArrayList
 
 	}
 
