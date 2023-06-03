db2070b83dcb1a9a2c4b8476d904408ed6015389
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-06-03 17:14:38 +0530

    Commit By FR

--- src/collection/Treeset.java
+++ src/collection/Treeset.java
@@ -11,6 +11,10 @@ public class Treeset extends LinkList {
 		tset.add(15);
 		tset.add(17);
 		System.out.println(tset);
+		// traverse using for Each Method...
+		tset.forEach(e->{                      // e-> used as lambda expression here it is noteworth the e can b e, str or anything desired variable of your choice...
+			System.out.println(e);
+		});
 	}
 
 }
