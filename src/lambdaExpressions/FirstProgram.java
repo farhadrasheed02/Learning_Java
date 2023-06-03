89e427b07d36814934a0db02cf838f943dbd8cad
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-06-04 00:27:11 +0530

    Commit By FR

--- src/lambdaExpressions/FirstProgram.java
+++ src/lambdaExpressions/FirstProgram.java
@@ -6,15 +6,25 @@ interface Drawable{
 public class FirstProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		FirstProgram fp = new FirstProgram();
 		
-		Drawable d2 = ()->{
-			
-			System.out.println("I am not drawing this");
-			
+		//Without Lambda Expression 
+		Drawable d2 = new Drawable() {
+			public void Draw()
+			{
+				System.out.println("I am drawing the painting");
+			}
 		};
 		d2.Draw();
+		
+		
+		
+		//Using Lambda Expression 
+//		Drawable d2 = ()->{
+//			
+//			System.out.println("I am not drawing this");
+//			
+//		};
+//		d2.Draw();
 	}
 
 }
