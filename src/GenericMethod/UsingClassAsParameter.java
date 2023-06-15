785dccc9bf540a95fa67c7431df40ee245c291cc
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-06-16 00:29:18 +0530

    Commit By FR

--- src/GenericMethod/UsingClassAsParameter.java
+++ src/GenericMethod/UsingClassAsParameter.java
@@ -1,19 +1,46 @@
 package GenericMethod;
 
+import java.util.ArrayList;
+
 public class UsingClassAsParameter {
 	
 	
 	public static void main(String args[])
 	{
+		ArrayList<String> str = new ArrayList<>();
+		str.add("Farhad");
+		str.add("Haseena");
+		showlist(str);
+		
+		
+		
+	}
+	public static void showlist(ArrayList<String> str)
+	{
+		for(String value : str)
+		{
+			System.out.println(value);
+		}
 		
 	}
 
 }
 class Machine extends Camera
 {
+
+	Camera machine = new Camera();
+	Machine mc = new Machine();
+	mc.
+	
 	
 }
 class Camera
 {
+	
+	public void seecam()
+	{
+		System.out.println("I can see cam ...");
+	}
+	
 	
 }
