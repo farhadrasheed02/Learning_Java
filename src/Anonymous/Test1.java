4bf328046b20a96d1ccd0e790c8f46c2b4b4082f
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-06-29 23:38:23 +0530

    Commit By FR

--- src/Anonymous/Test1.java
+++ src/Anonymous/Test1.java
@@ -5,10 +5,17 @@ class A
 	{
 		System.out.println("Machine is working according to it's manual....");
 	}
+} 
+
+	interface cricket
+{
+	public void teamind();
 }
 public class Test1 {
 	public static void main(String[] args) {
+//		a.start();
 		A a = new A()
+		
 		{
 		@Override
 		public void start()	
@@ -19,9 +26,20 @@ public class Test1 {
 		}
 		};
 		a.start();
-	}
 	
+	
+	cricket cric = new cricket()
+			{
+
+				@Override
+				public void teamind() {
+					// TODO Auto-generated method stub
+					System.out.println("What is going on in the team right now");
+				}
+			
+			};
+			cric.teamind();
 		
 	}
-
+}
 
