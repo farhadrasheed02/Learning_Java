93fbcf54c7b463bfd7f20753469b7a6b1dc0f941
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-07-02 22:45:45 +0530

    Commit By FR

---
+++ src/ExceptionHandling/handle1.java
@@ -0,0 +1,13 @@
+package ExceptionHandling;
+
+import java.io.File;
+
+public class handle1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File file = new File("");
+		
+	}
+
+}
