364a820acf7845559b200f461c130f7f2dbb75b4
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-07-02 23:08:12 +0530

    Commit by FR

--- src/ExceptionHandling/handle1.java
+++ src/ExceptionHandling/handle1.java
@@ -1,12 +1,23 @@
 package ExceptionHandling;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class handle1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		File file = new File("");
+		File file = new File("..\\Learning_Java\\src\\file.txt");
+		
+		try {
+			FileReader fr = new FileReader(file);
+			
+			// this will not be executed if an exception is thrown
+			System.out.println("Continuing.........");
+		} catch (Exception e) {
+			System.out.println("File not found "+e.getMessage());
+		}
 		
 	}
 
