4150fbe7d9003e778514c16703f3971641328a26
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-07-03 00:50:42 +0530

    Commit by FR

---
+++ src/ExceptionHandling/multipleexception.java
@@ -0,0 +1,10 @@
+package ExceptionHandling;
+
+public class multipleexception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("In sha Allah working tommorrow");
+	}
+
+}
