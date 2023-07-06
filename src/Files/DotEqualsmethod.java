4745c8ba79f61accd158ea0b220595561038cdb3
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-07-07 00:29:59 +0530

    Commit By FR

---
+++ src/Files/DotEqualsmethod.java
@@ -0,0 +1,13 @@
+package Files;
+
+public class DotEqualsmethod {
+	public static String farhad = "farhad";
+	public static String farhad1 = "farhad";
+
+	public static void main(String[] args) {
+		
+		System.out.println(farhad.equals(farhad1));
+		
+	}
+
+}
