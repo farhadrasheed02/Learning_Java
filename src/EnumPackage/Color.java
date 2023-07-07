b33a18c072bbd928526a8e729520f95bc4a43678
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-07-08 00:32:08 +0530

    Commit By FR

--- src/EnumPackage/Color.java
+++ src/EnumPackage/Color.java
@@ -1,8 +1,21 @@
 package EnumPackage;
 
 public enum Color {
- RED,
- GREEN,
- BLUE,
- WHITE
+ RED("Hasu"),
+ GREEN("Farhad"),
+ BLUE("Love"),
+ WHITE("Peace");
+	
+	private String name;
+	private Color(String name) {
+		this.name=name;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	public String toString()
+	{
+		return "This Color is called " +name;
+	}
 }
--- src/EnumPackage/MainClass.java
+++ src/EnumPackage/MainClass.java
@@ -5,12 +5,12 @@ public class MainClass {
 	public static void main(String[] args) {
 		
 		 color= Color.BLUE;
-		color=Color.RED;
+		
 			
 		switch(color)
 		{
 		case BLUE:
-			System.out.println("This is the "+ color+" from Color Class");
+			System.out.println("This is the blue color from Color Class");
 			break;
 		case GREEN:
 			System.out.println("This is the "+ color+" from Color Class");
@@ -25,8 +25,13 @@ public class MainClass {
 			break;
 
 		
-		}
-		System.out.println(Color.RED.getClass());
+	}
+		System.out.println(Color.RED);
+		System.out.println(Color.BLUE.getName());
+		System.out.println(Color.GREEN.toString());
+		System.out.println("Enum name as a String "+Color.RED.name());
+		//System.out.println(Color.RED.getClass());
+		//System.out.println(Color.GREEN instanceof Color);  // is it actually instance of anything..it should print true...
 		
 	}
 	
