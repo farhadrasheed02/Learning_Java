d7d5944a81c516de83fa26931690d7c968178f42
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-07-10 23:01:24 +0530

    Commit By FR

--- src/udemyJavaCollection/ArrayListEasyWay.java
+++ src/udemyJavaCollection/ArrayListEasyWay.java
@@ -1,10 +1,20 @@
 package udemyJavaCollection;
 
+import java.util.ArrayList;
+
 public class ArrayListEasyWay {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		ArrayList<Integer> numbers = new ArrayList<>();
+		numbers.add(10);
+		numbers.add(20);
+		numbers.add(30);
+		numbers.add(100);
+		for(Integer integer : numbers)
+		{
+			System.out.println(integer);
+		}
 	}
 
 }
