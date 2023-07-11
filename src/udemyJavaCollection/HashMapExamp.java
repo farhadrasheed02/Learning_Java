d5d38c72199a49aaedae3af27c3ec54cfb90facb
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-07-11 22:50:32 +0530

    Commit By FR

---
+++ src/udemyJavaCollection/HashMapExamp.java
@@ -0,0 +1,14 @@
+package udemyJavaCollection;
+
+import java.util.HashMap;
+
+public class HashMapExamp {
+
+	public static void main(String[] args) {
+
+		HashMap< String, Integer> hashMap = new HashMap<>();
+		hashMap.put("Farhad",20);
+		System.out.println(hashMap);
+	}
+
+}
