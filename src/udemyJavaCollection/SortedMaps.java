449b76c330941fa839f5498671e1c7b35ae38caf
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-07-14 01:34:58 +0530

    Commit By FR

--- src/udemyJavaCollection/SortedMaps.java
+++ src/udemyJavaCollection/SortedMaps.java
@@ -1,9 +1,39 @@
 package udemyJavaCollection;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class SortedMaps {
 
 	public static void main(String[] args) {
 
+		// John from www.caveofprogramming.com
+		HashMap<Integer, String> hMap = new HashMap<>();
+		TreeMap<Integer, String> tMap = new TreeMap<>();
+		LinkedHashMap<Integer, String> lMap = new LinkedHashMap<>();
+		System.out.println("HashMap order now :");
+		testMap(hMap);
+		System.out.println("TreeMap order  in natural way:");
+		testMap(tMap);
+		System.out.println("LinkedMap order prints the same order in which we have provided the value :");
+		testMap(lMap);
+		
+	}
+	
+	public static void testMap(Map<Integer,String> map)
+	{
+		map.put(10, "farhad");
+		map.put(0, "Jani");
+		map.put(5, "Hani");
+		map.put(6, "Pani");
+		map.put(4, "Mani");
+		for(Integer key :map.keySet())
+		{
+			String value = map.get(key);
+			System.out.println(key +": "+value);
+		}
 	}
 
 }
