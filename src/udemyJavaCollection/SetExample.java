9570e03d56a7548a2c7ad9107ed55154ed97ad34
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-07-15 00:51:39 +0530

    Commit by FR

--- src/udemyJavaCollection/SetExample.java
+++ src/udemyJavaCollection/SetExample.java
@@ -1,13 +1,14 @@
 package udemyJavaCollection;
 
-import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class SetExample {
 
 	public static void main(String[] args) {
 		
+		// HashSet doesnot retain order..... and does not allow duplicate as well
 		Set<String> set = new HashSet<String>();
 			set.add("Farhad");
 			set.add("Rashid");
@@ -15,8 +16,11 @@ public class SetExample {
 			set.add("Sultan");
 			set.add("Rasheed");
 			set.add("Reshi");
-			System.out.println(set);
-			for(String str : set)
+			
+			Set<String> list = new LinkedHashSet<>(set);
+			list.add("Farhad");
+			//System.out.println(list);
+			for(String str : list)
 			{
 				System.out.println(str);
 			}
