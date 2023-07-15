3e5589df78ab5701aaa70d962aab3c3a523ca825
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-07-16 01:39:51 +0530

    Commit By FR

--- src/udemyJavaCollection/ComparaTorClass.java
+++ src/udemyJavaCollection/ComparaTorClass.java
@@ -1,8 +1,30 @@
 package udemyJavaCollection;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class StringLengthComparator implements Comparator<String>
+{
+	public int compare(String str1, String str2)
+	{
+		return 0;
+		
+	}
+}
 public class ComparaTorClass {
 
 	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<>();
+		list.add("Farhad");
+		list.add("Rashid");
+		list.add("Reshi");
+		list.add("Haseena");
+		list.add("Nighat");
+		list.add("Khan");
+		
+		
 		
 		
 	}
