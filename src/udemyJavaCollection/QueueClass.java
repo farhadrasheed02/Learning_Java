445cc78e07f84cdcdb14b7cf5f29d23132688a0d
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-07-17 01:31:31 +0530

    Commit By FR

--- src/udemyJavaCollection/QueueClass.java
+++ src/udemyJavaCollection/QueueClass.java
@@ -1,9 +1,40 @@
 package udemyJavaCollection;
 
+import java.util.NoSuchElementException;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class QueueClass {
 
 	public static void main(String[] args) {
-
-	}
+		try {
+		Queue<Integer> queue = new ArrayBlockingQueue<>(3);
+		queue.add(20);
+		queue.add(40);
+		queue.add(80);
+		
+		System.out.println("The head of the Queue is "+queue.element());
+		for(Integer integer : queue)
+		
+			System.out.println(integer);		
+		}
+		catch(IllegalStateException e)
+		{
+			System.out.println("The size of the queue is smaller than its elments added"+e.toString());
+		}
+		catch(NoSuchElementException e)
+		{
+			System.out.println("The  element in the Queue is  "+e.getMessage());
+		}
+		}
 
 }
+
+
+
+
+
+
+
+
+
