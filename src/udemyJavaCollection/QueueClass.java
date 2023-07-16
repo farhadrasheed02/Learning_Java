f7e7c9c799ddc673acd2628be4fb73b0e7ae03b7
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-07-17 01:39:33 +0530

    Commit By FR

--- src/udemyJavaCollection/QueueClass.java
+++ src/udemyJavaCollection/QueueClass.java
@@ -11,13 +11,17 @@ public class QueueClass {
 		Queue<Integer> queue = new ArrayBlockingQueue<>(3);
 		queue.add(20);
 		queue.add(40);
-		queue.add(80);
+//		queue.add(80);
+		if(queue.offer(10)==false)
+		{
+			System.out.println("offer failed to add in the queue");
+		}
 		
 		System.out.println("The head of the Queue is "+queue.element());
 		for(Integer integer : queue)
 		
 			System.out.println(integer);		
-		System.out.println(queue.peek());
+		System.out.println("The peek/head of the queue is "+queue.peek());
 		}
 		
 		catch(IllegalStateException e)
