1bfc7cb98b34d187e9cd1ed29b7450688a64ffd3
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-07-18 01:31:22 +0530

    Commit By FR

--- src/udemyJavaCollection/IteratorClass.java
+++ src/udemyJavaCollection/IteratorClass.java
@@ -3,6 +3,7 @@ package udemyJavaCollection;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class IteratorClass {
 
@@ -15,14 +16,21 @@ public class IteratorClass {
 		list.add("Haseena");
 		list.add("Nighat");
 		list.add("Khan");
-		Iterator<String> iterator = list.iterator();
-		while(iterator.hasNext())
+//		Iterator<String> iterator = list.iterator();
+		ListIterator<String> literator = list.listIterator(); // allows you to add element in run time as well...
+		while(literator.hasNext())
 		{
-			String str = iterator.next();
-			System.out.println(str);
+			String str = literator.next();
+			
+			if(str.equals("Farhad"))
+				literator.add("Jan Soub");
+			if(str.equals("Haseena"))
+				literator.add("Farhad's Wife");
+			
+//			System.out.println(str);
 		}
 		
-		
+		System.out.println(list);
 		
 	}
 
