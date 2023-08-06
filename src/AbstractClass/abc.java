e85501e97fdbaf4a20d96c02ff789ce5211beaef
farhadrasheed02 <42071670+farhadrasheed02@users.noreply.github.com>
2023-08-06 22:50:44 +0530

    Commit By FR

--- src/AbstractClass/abc.java
+++ src/AbstractClass/abc.java
@@ -3,5 +3,6 @@ package AbstractClass;
 public class abc {
 	public static void main(String[] args) {
 		System.out.println("I am working on this topic definately........");
+		System.out.println("Dreams do come true....");
 	}
 }
