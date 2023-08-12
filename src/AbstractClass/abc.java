fda41812860b217431744b43fcfef7aba9817c33
farhadrasheed02 <42071670+farhadrasheed02@users.noreply.github.com>
2023-08-12 22:11:30 +0530

    Commit By FR

--- src/AbstractClass/abc.java
+++ src/AbstractClass/abc.java
@@ -4,5 +4,6 @@ public class abc {
 	public static void main(String[] args) {
 		System.out.println("I am working on this topic definately........");
 		System.out.println("Dreams do come true....");
+		System.out.println("Today Is my Birthday that is 12 of August 2023");
 	}
 }
