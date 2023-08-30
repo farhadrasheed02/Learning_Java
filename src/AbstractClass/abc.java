4d3cb53f0dfda49161b80a49458489791079f19c
farhadrasheed02 <42071670+farhadrasheed02@users.noreply.github.com>
2023-08-30 23:52:42 +0530

    commit by FR

--- src/AbstractClass/abc.java
+++ src/AbstractClass/abc.java
@@ -10,7 +10,7 @@ public class abc {
 		System.out.println("To keep the things consisent even a single comment matters in that");
 		System.out.println("parents are blessing to us keep it in mind");
 		System.out.println("Continuing the process");
-		System.out.println("I have become lazy...");
+		System.out.println("I have become lazy.........");
 		System.out.println("another day to continue. and today is there another day..and this will ..");
 		System.out.println("Another day another commit right....");
 		System.out.println("To be continue.................................AD CWS (Application Developement Client Web Services) ");
