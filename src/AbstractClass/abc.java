f898d549c33836d04d75502f0b983a683d2924e3
farhadrasheed02 <42071670+farhadrasheed02@users.noreply.github.com>
2023-09-06 23:33:21 +0530

    Commit By FR

--- src/AbstractClass/abc.java
+++ src/AbstractClass/abc.java
@@ -2,6 +2,15 @@ package AbstractClass;
 
 public class abc {
 	public static void main(String[] args) {
-		
+		int i;
+		int arr[] = {4,8,9,10,15};
+		for(i=0;i<arr.length;i++)
+		{
+			if(arr[i]%2==0)
+			{
+			System.out.println(arr[i]);
+			}
+			
+		}
 	}
 }
