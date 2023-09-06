ccf80b9914d9fc023a905b908996c1b543d7e0b3
farhadrasheed02 <42071670+farhadrasheed02@users.noreply.github.com>
2023-09-06 23:21:44 +0530

    Commit By FR

--- src/AbstractClass/abc.java
+++ src/AbstractClass/abc.java
@@ -2,17 +2,6 @@ package AbstractClass;
 
 public class abc {
 	public static void main(String[] args) {
-		System.out.println("I am working on this topic definately........");
-		System.out.println("Dreams do come true....");
-		System.out.println("Today Is my Birthday that is 12 of August 2023");
-		System.out.println("Today is International Left Handers Day");
-		System.out.println("To keep the things consisent even a single comment matters in that");
-		System.out.println("To keep the things consisent even a single comment matters in that");
-		System.out.println("parents are blessing to us keep it in mind");
-		System.out.println("Continuing the process of overcoming................");
-		System.out.println("I have become lazy.and bulky as well.Something is wrong bro............");
-		System.out.println("another day to continue. and today is there another day..and this will ..");
-		System.out.println("Another day another commit right....");
-		System.out.println("To be continue.................................AD CWS (Application Developement Client Web Services) ");
+		
 	}
 }
