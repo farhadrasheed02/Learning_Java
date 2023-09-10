ce6cd320cd167e998591dae25bb3f1fcb6b7d33f
farhadrasheed02 <42071670+farhadrasheed02@users.noreply.github.com>
2023-09-10 12:32:02 +0530

    Commit By FR

--- src/AbstractClass/abc.java
+++ src/AbstractClass/abc.java
@@ -3,7 +3,7 @@ package AbstractClass;
 public class abc {
 	public static void main(String[] args) {
 		int i;
-		System.out.println("Working with Selenium");
+		System.out.println("Working with Selenium...");
 		int arr[] = {4,8,9,10,15};
 		for(i=0;i<arr.length;i++)
 		{
